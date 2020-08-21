bd883df3d5335cf17bec8951feb9f63cc3191394
alusmartinez <dalej.martinez@gmail.com>
2020-08-21 12:00:24 -0300

    Create EjercicioN2-TP11.js

---
+++ Clase-#14-140820/EjercicioN2-TP11.js
@@ -0,0 +1,17 @@
+//Ejercicio N° 2- TP11 String II
+function comprobarCaracter(frase,caracter){
+	var indice= 0;
+	var resultado;	
+	
+	while(frase[indice] != '.'){ //Controla el final de la cadena
+		while(frase[indice] == ' '){ //Pasa por alto los espacios en blanco
+			indice++;
+		}
+		resultado='';		
+		if(frase[indice] == caracter){
+			resultado='Se encontró el caracter en la posición '+ indice;
+			return resultado;
+		}
+		indice++;
+    }
+}
